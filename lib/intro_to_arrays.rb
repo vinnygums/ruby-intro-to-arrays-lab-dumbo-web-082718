4beea24363a0dec52fde4096c7ec49eb8bf98bb9
Vicente Soriano <vicente.soriano@icloud.com>
2018-08-18 19:54:19 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -17,3 +17,13 @@ end
 def last_element(array)
   array[-1]
 end
+
+def first_element_with_array_methods(array)
+end
+  
+def last_element_with_array_methods(array)
+end
+
+def length_of_array(array)
+  array.length
+end
